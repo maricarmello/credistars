1d2ceb3c95f9e4bd3dee3c6940062db6b1e3e64e
mariana.carmello <mariana.carmello@creditas.com.br>
2020-05-07 17:18:52 -0300

    Adding transactions to database

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,12 +1,28 @@
 const User = require('../models/user');
+const Transaction = require('../models/transaction')
+let currentUserId = 1;
 
 module.exports = {
+  
   dashboard: async (request, response) => {
-    // possibleReceivers(1) é o id da pessoa logada
-    let users = await User.possibleReceivers(2);
-    return response.view('home/dashboard', { users: users });
+
+    let users = await User.possibleReceivers(currentUserId);
+
+    let transactions = await Transaction.all();
+
+    return response.view('home/dashboard', { users: users, transactions: transactions });
   },
   sendStars: async(request, response) =>{
+
+    let transaction = {
+      user_id_sender: currentUserId,
+      user_id_receiver: request.payload.user_id_receiver,
+      quantity: request.payload.quantity,
+      massage: request.payload.massage,
+      value: request.payload.value,
+      date: new Date()
+    };
+    await Transaction.create(transaction);
     return response.redirect('/home')
   }
 }
--- src/models/transaction.js
+++ src/models/transaction.js
@@ -1,15 +1,26 @@
 const dbConnection = require('../db_connection');
 
-class transaction {
-    constructor(attrs){
-      this.transaction_id = attrs.transaction_id;
-      this.user_id_sender = attrs.user_id_sender;
-      this.user_id_receiver = attrs.user_id_receiver;
-      this.quantity = attrs.quantity;
-      this.massage = attrs.massage;
-      this.value = attrs.value;
-      this.date = attrs.date;
-
-  
-    };
+class Transaction {
+constructor(attrs){
+    this.transaction_id = attrs.transaction_id;
+    this.user_id_sender = attrs.user_id_sender;
+    this.user_id_receiver = attrs.user_id_receiver;
+    this.quantity = attrs.quantity;
+    this.massage = attrs.massage;
+    this.value = attrs.value;
+    this.date = attrs.date;
+};
+static async all() {
+let data = await dbConnection.query("SELECT * FROM `transactions`", { type: dbConnection.QueryTypes.SELECT });
+return data.map((attr) => new Transaction(attr));
 }
+static async create(attrs) {
+    await dbConnection.query(`INSERT INTO transactions (user_id_sender, user_id_receiver, quantity, message, value, date) 
+        VALUES ("${attrs.user_id_sender}", "${attrs.user_id_receiver}", "${attrs.quantity}", "${attrs.message}", "${attrs.value}", "${attrs.date}")`, { type: dbConnection.QueryTypes.INSERT });
+
+    return true;
+  }
+}
+
+
+module.exports = Transaction;
