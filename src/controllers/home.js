61488b53fec620d598c87c13084545043eab57e2
mariana.carmello <mariana.carmello@creditas.com.br>
2020-05-08 17:29:11 -0300

    Fixing the name and date on the feed

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const Transaction = require('../models/transaction')
+
 let currentUserId = 1;
 
 module.exports = {
@@ -15,6 +16,13 @@ module.exports = {
     return response.view('home/dashboard', { currentUser:currentUser, users: users, transactions: transactions });
   },
   sendStars: async(request, response) =>{
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+
 
     let transaction = {
       user_id_sender: currentUserId,
@@ -22,7 +30,7 @@ module.exports = {
       quantity: request.payload.quantity,
       message: request.payload.message,
       value: request.payload.value,
-      date: new Date()
+      date: day + "/" + (month+1) + "/" + year + " as " + hour + ':' + min 
     };
     await Transaction.create(transaction);
     return response.redirect('/home')
--- src/models/transaction.js
+++ src/models/transaction.js
@@ -3,17 +3,23 @@ const dbConnection = require('../db_connection');
 class Transaction {
 constructor(attrs){
     this.transaction_id = attrs.transaction_id;
-    this.user_id_sender = attrs.user_id_sender;
-    this.user_id_receiver = attrs.user_id_receiver;
+    this.sender = attrs.sender;
+    this.receiver = attrs.receiver;
     this.quantity = attrs.quantity;
     this.message = attrs.message;
     this.value = attrs.value;
     this.date = attrs.date;
 };
 static async all() {
-let data = await dbConnection.query("SELECT * FROM `transactions` ORDER BY transaction_id DESC", { type: dbConnection.QueryTypes.SELECT });
+let data = await dbConnection.query(
+    "SELECT t.transaction_id, us.name as sender, ur.name as receiver, t.quantity, t.message, t.value, t.date " + 
+    "FROM transactions t " +
+    "INNER JOIN users us ON us.user_id = t.user_id_sender " +
+    "INNER JOIN users ur ON ur.user_id = t.user_id_receiver " +
+    "ORDER BY transaction_id DESC", { type: dbConnection.QueryTypes.SELECT });
 return data.map((attr) => new Transaction(attr));
 }
+
 static async create(attrs) {
     await dbConnection.query(`INSERT INTO transactions (user_id_sender, user_id_receiver, quantity, message, value, date) 
         VALUES ("${attrs.user_id_sender}", "${attrs.user_id_receiver}", "${attrs.quantity}", "${attrs.message}", "${attrs.value}", "${attrs.date}")`, { type: dbConnection.QueryTypes.INSERT });
